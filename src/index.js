04dfa0954d6ed3c7dd3ba9cd0639d8b4f5b533b4
Nel-Osare <ncastro@osarecr.com>
2026-03-02 19:10:21 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+/**
+ * ARCHIVO DE ENTRADA (LANZADOR)
+ * Este archivo conecta el componente App.js con el div "root" de index.html
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
