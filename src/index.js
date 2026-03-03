e449942679d4abcdc7dca37cc782b4da10021da6
Nel-Osare <ncastro@osarecr.com>
2026-03-02 19:15:39 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import App from './App';
 
 /**
  * ARCHIVO DE ENTRADA (LANZADOR)
- * Este archivo conecta el componente App.js con el div "root" de index.html
+ * Este archivo conecta el componente App.js con el div "root" de index.html.
+ * Se ha verificado que no existan caracteres invisibles que causen errores en Vercel.
  */
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!rootElement) {
+  console.error("No se encontró el elemento con id 'root'. Asegúrate de que public/index.html lo incluya.");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
