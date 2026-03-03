b6877d35f0b6887ab37ea42c721abc65c4a1ffed
Nel-Osare <ncastro@osarecr.com>
2026-03-02 23:04:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Este archivo es el punto de entrada que conecta el componente App
+// con el elemento 'root' definido en tu archivo public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
